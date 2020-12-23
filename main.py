75a515e9d6de8895dce8cfefbd07848c0f52176a
Bing <sbbmu@hotmail.com>
2020-12-23 19:50:04 +0000

    1

--- main.py
+++ main.py
@@ -20,21 +20,50 @@ for i in range(6):
 # game variables
 hangman_status  = 0 # which image to draw
 
-#colors
+# button variables
+RADIUS = 20
+GAP = 15
+letters = []
+startX = round((WIDTH - (RADIUS *2 +GAP) *13) / 2)
+startY = 400
+for i in range(26):
+  x = startX + GAP * 2 + (RADIUS * 2 + GAP) * (i % 13)
+  y = startY + (RADIUS * 2 + GAP) * (i // 13)
+  letters.append([x, y, chr(65 + i)])
+
+# colors
 WHITE = (255,255,255)
+BLACK = (0,0,0)
+
+# fonts
+LETTER_FONT = pygame.font.Sysfont('comicsans', 40)
 
 # setup game loop
 FPS = 60
 clock = pygame.time.Clock()
 run = True
 
+def draw():
+  win.fill(WHITE)
+  
+  # draw buttons
+  for pos in letters:
+    x, y, letter = pos
+    pygame.draw.circle(win, BLACK, (x,y), RADIUS, 3)
+    text = LETTER_FONT.render(letter, 1, BLACK)
+    win.blit(text, (x,y))
+  
+  win.blit(images[hangman_status], (150, 100))
+  pygame.display.update()
+  
+  
 while run:
     clock.tick(FPS)
     
-    win.fill(WHITE)
-    win.blit(images[hangman_status], (150, 100))
-    pygame.display.update()
-
+    # display    
+    draw()
+    
+    # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
